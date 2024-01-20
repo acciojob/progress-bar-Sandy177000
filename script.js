3e7323d899b0fb9d24082d102a37dfcfc0b6dc8d
Sandesh <112424645+Sandy177000@users.noreply.github.com>
2024-01-20 16:28:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+let index = 1;
+
+function prev() {
+  if (index > 1) {
+    let curr = document.getElementById(`${index}`);
+    index--;
+    curr.classList.remove("active");
+  }
+}
+
+function next() {
+  if (index < 5) {
+    index++;
+    let curr = document.getElementById(`${index}`);
+    curr.classList.add("active");
+    console.log(curr);
+  }
+}
